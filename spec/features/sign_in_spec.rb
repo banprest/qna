0947a200cfc101f4c7f77e5fea25556b962f735c
banpresto <sexyteemo@gmail.com>
2021-03-04 22:39:01 +0300

    relize feature 'User can sign_in'

---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+feature 'User can sign_in', %q{
+  In order to ask questions
+  As an unauthenticated user
+  I'd like to be able to sing in
+} do
+
+  given(:user) { User.create(email: 'test@mail.ru', password: '123456') }
+
+  background { visit new_user_session_path }
+
+  scenario 'Registred user tries to sign in' do
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_on 'Log in'
+
+    expect(page).to have_content 'Signed in successfully'
+  end
+
+  scenario 'Unregistred user tries to sign in' do
+    fill_in 'Email', with: 'wrong@mail.ru'
+    fill_in 'Password', with: '123456'
+    click_on 'Log in'
+
+    expect(page).to have_content 'Invalid Email or password'
+  end
+
+end
