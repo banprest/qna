a6d8f80ac59c3567d629e9c4a6bebe31092edda2
banpresto <sexyteemo@gmail.com>
2021-03-20 18:07:26 +0300

    Gist

--- spec/factories/links.rb
+++ spec/factories/links.rb
@@ -6,5 +6,9 @@ FactoryBot.define do
     trait :invalid do
       url { '123' }
     end
+
+    trait :gist do
+      url { 'https://gist.github.com/banprest/16a49fd807bb77d0032306d78554d4bd'}
+    end
   end
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -7,4 +7,19 @@ RSpec.describe Link, type: :model do
   it { should validate_presence_of :url }
 
   it { should allow_value('https://www.google.ru/').for(:url) }
+
+  describe 'Link gist?' do
+    let(:user) { create(:user) }
+    let(:question) { create(:question, user: user) }
+    let(:link) { create(:link, :gist, linkable: question)}
+    let(:link1) { create(:link, linkable: question)}
+    
+    it 'true' do
+      expect(link).to be_gist
+    end
+
+    it 'false' do
+      expect(link1).to_not be_gist
+    end
+  end
 end
